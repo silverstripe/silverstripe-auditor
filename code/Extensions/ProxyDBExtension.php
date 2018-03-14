1e2ef351a1a17476595fca851ae7f0c42050c9e9
Robbie Averill <robbie@silverstripe.com>
2018-03-14 17:16:19 +1300

    NEW Implement Database manipulate() method proxy for audit hook

---
+++ code/Extensions/ProxyDBExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SilverStripe\Auditor\Extensions;
+
+use SilverStripe\Auditor\AuditHook;
+use SilverStripe\Core\Extension;
+use TractorCow\ClassProxy\Generators\ProxyGenerator;
+
+class ProxyDBExtension extends Extension
+{
+    /**
+     * Bind a proxy callback into the Database::manipulate method to allow us to track database activity
+     * for the {@link AuditHook} class
+     *
+     * @param ProxyGenerator $proxy
+     */
+    public function updateProxy(ProxyGenerator &$proxy)
+    {
+        $proxy = $proxy->addMethod('manipulate', function ($args, $next) {
+            $manipulation = $args[0];
+            AuditHook::handle_manipulation($manipulation);
+            return $next(...$args);
+        });
+    }
+}

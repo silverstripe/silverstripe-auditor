e5162c86be7156cb1bd966cf076a9dcaf6429a96
Sabina Talipova <87288324+sabina-talipova@users.noreply.github.com>
2023-01-18 12:23:59 +1300

    DEP PHP Support in CMS5 (#47)

--- code/RealIPProcessor.php
+++ code/RealIPProcessor.php
@@ -2,12 +2,13 @@
 
 namespace SilverStripe\Auditor;
 
+use Monolog\LogRecord;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Core\Injector\Injector;
 
 class RealIPProcessor
 {
-    public function __invoke(array $record)
+    public function __invoke(LogRecord $record)
     {
         $req = Injector::inst()->create(HTTPRequest::class, null, null);
         $record['extra']['real_ip'] = $req->getIP();
